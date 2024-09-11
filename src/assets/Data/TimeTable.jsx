7c064ae48544a33c08363d7550dc8131f606f69d
o0PHOENIX0o <pheonixyt5@gmail.com>
2024-09-11 21:06:02 +0530

    timetable edit

--- src/assets/Data/TimeTable.jsx
+++ src/assets/Data/TimeTable.jsx
@@ -1,285 +1,292 @@
 const ScheduleData = {
-    "Monday": [
-      {
-        "A": {
-          "course": "ICT 257 / DTL318",
-          "name": "Operating Systems Lab",
-          "shortName": "OS",
-          "Faculty": "RKP",
-          "start": "9:00 AM",
-          "end": "11:00 AM"
-        },
-        "B": {
-          "course": "ICT 255 / DTL217",
-          "name": "Data Structure Lab",
-          "shortName": "DS LAB",
-          "Faculty": "RSPOOJA",
-          "start": "9:00 AM",
-          "end": "11:00 AM"
-        }
-      },
-      {
-        "course": "ICT 203 / E111",
-        "name": "Operating Systems",
+  "Monday": [
+    {
+      "A": {
+        "course": "ICT 257 / DTL318",
+        "name": "Operating Systems Lab",
         "shortName": "OS",
         "Faculty": "RKP",
-        "start": "11:00 AM",
-        "end": "1:00 PM"
+        "start": "9:00 AM",
+        "end": "11:00 AM"
       },
-      {
-        "course": "",
-        "name": "Break",
-        "start": "1:00 PM",
-        "end": "1:30 PM"
+
+      "B": {
+        "course": "ICT 255 / DTL217",
+        "name": "Data Structure Lab",
+        "shortName": "DS LAB",
+        "Faculty": "RSPOOJA",
+        "start": "9:00 AM",
+        "end": "11:00 AM"
+      }
+    },
+    {
+      "course": "ICT 203 / E111",
+      "name": "Operating Systems",
+      "shortName": "OS",
+      "Faculty": "RKP",
+      "start": "11:00 AM",
+      "end": "1:00 PM"
+    },
+    {
+      "course": "",
+      "name": "Break",
+      "start": "1:00 PM",
+      "end": "1:30 PM"
+    },
+    {
+      "A": {
+        "course": "ICT 255 / ETL219",
+        "name": "Data Structure Lab",
+        "shortName": "DS LAB",
+        "Faculty": "TBA",
+        "start": "1:30 PM",
+        "end": "3:30 PM"
       },
-      {
-        "A": {
-          "course": "ICT 255 / ETL219",
-          "name": "Data Structure Lab",
-          "shortName": "DS LAB",
-          "Faculty": "TBA",
-          "start": "1:30 PM",
-          "end": "3:30 PM"
-        },
-        "B": {
-          "course": "ICT 251 / DTL318",
-          "name": "Database Management System Lab",
-          "shortName": "DBMS LAB",
-          "Faculty": "TBA",
-          "start": "3:30 PM",
-          "end": "4:30 PM"
-        }
+
+      "B": {
+        "course": "ICT 251 / DTL318",
+        "name": "Database Management System Lab",
+        "shortName": "DBMS LAB",
+        "Faculty": "TBA",
+        "start": "1:30 PM",
+        "end": "3:30 PM"
+      }
+    },
+    {
+      "course": "ICT 205 / E111",
+      "name": "Object Oriented Programming Using C++ ",
+      "shortName": "OOPS LAB",
+      "Faculty": "Ms Priyanka Bhutani",
+      "start": "3;:30 PM",
+      "end": "04:30 PM"
+    }
+  ],
+  "Tuesday": [
+    {
+      "course": "ICT 205 / E111",
+      "name": "Digital Logic and Computer Design ",
+      "shortName": "DLCD",
+      "Faculty": "Ms. P. Choudhary",
+      "start": "9:00 AM",
+      "end": "11:00 AM"
+    },
+    {
+      "course": "ICT 211 / E111",
+      "name": "Data Structures",
+      "shortName": "DS",
+      "Faculty": "Navin Rajpal",
+      "start": "11:00 AM",
+      "end": "1:00 PM"
+    },
+    {
+      "course": "",
+      "name": "Break",
+      "start": "1:00 PM",
+      "end": "1:30 PM"
+    },
+    {
+      "course": "ECO 213 / E111",
+      "name": "Engineering Economics",
+      "shortName": "ECO",
+      "Faculty": "---",
+      "start": "1:30 PM",
+      "end": "3:30 PM"
+    },
+    {
+      "course": "ICT 207 / E111",
+      "name": " Database Management System",
+      "shortName": "DBMS",
+      "Faculty": "TBA",
+      "start": "3:30 PM",
+      "end": "5:30 PM"
+    }
+  ],
+  "Wednesday": [
+
+    {
+      "course": "BC 123 / E111",
+      "name": "Bridge Course in Mathematics",
+      "shortName": "Maths",
+      "Faculty": "Nikhat Ali",
+      "start": "9:00 AM",
+      "end": "11:00 AM"
+    },
+   
+    {
+      "course": "ICT 209 / E111",
+      "name": "Object Oriented Programming Using C++",
+      "shortName": "OOPS",
+      "Faculty": "Ms Priyanka Bhutani",
+      "start": "11:00 AM",
+      "end": "12:00 PM"
+    },
+     {
+      "course": "ICT 201 / E111",
+      "name": "Foundations of Computer Science",
+      "shortName": "FCS",
+      "Faculty": "Dr. Rinkaj Goyal",
+      "start": "12:00 PM",
+      "end": "1:00 PM"
+    },
+    {
+      "course": "",
+      "name": "Break",
+      "start": "1:00 PM",
+      "end": "1:30 PM"
+    },
+    {
+      "A": {
+        "course": "ICT 253 / ETL506",
+        "name": "Object Oriented programming using C++ Lab",
+        "shortName": "OOPS LAB",
+        "Faculty": "Ms Priyanka Bhutani",
+        "start": "1:30 PM",
+        "end": "3:30 PM"
       },
-      {
-        "course": "ICT 205 / E111",
-        "name": "Object Oriented Programming Using C++ ",
+      "B": {
+        "course": "ICT 257 / ETL312",
+        "name": "Operating Systems Lab ",
+        "shortName": "OS LAB ",
+        "Faculty": "---",
+        "start": "1:30 PM",
+        "end": "3:30 PM"
+      }
+    },
+    {
+      "A": {
+        "course": "ICT 251 / DTL317",
+        "name": "Database Management System Lab",
+        "shortName": "DBMS Lab",
+        "Faculty": "TBA",
+        "start": "3:30 PM",
+        "end": "5:30 PM"
+      },
+      "B": {
+        "course": "ICT 253 / ETL313",
+        "name": "Object Oriented programming using C++ Lab",
         "shortName": "OOPS LAB",
         "Faculty": "Ms Priyanka Bhutani",
         "start": "3:30 PM",
-        "end": "04:30 PM"
-      }
-    ],
-    "Tuesday": [
-      {
-        "course": "ICT 205 / E111",
-        "name": "Digital Logic and Computer Design ",
-        "shortName": "DLCD",
-        "Faculty": "Ms. P. Choudhary",
-        "start": "9:00 AM",
-        "end": "11:00 AM"
-      },
-      {
-        "course": "ICT 211 / E111",
-        "name": "Data Structures",
-        "shortName": "DS",
-        "Faculty": "Navin Rajpal",
-        "start": "11:00 AM",
-        "end": "1:00 PM"
-      },
-      {
-        "course": "",
-        "name": "Break",
-        "start": "1:00 PM",
-        "end": "1:30 PM"
-      },
-      {
-        "course": "ECO 213 / E111",
-        "name": "Engineering Economics",
-        "shortName": "ECO",
-        "Faculty": "---",
-        "start": "1:30 PM",
-        "end": "3:30 PM"
-      },
-      {
-        "course": "ICT 207 / E111",
-        "name": " Database Management System",
-        "shortName": "DBMS",
-        "Faculty": "TBA",
-        "start": "3:30 PM",
         "end": "5:30 PM"
       }
-    ],
-    "Wednesday": [
-      {
-        "course": "BC 123 / E111",
-        "name": "Bridge Course in Mathematics",
-        "shortName": "Maths",
-        "Faculty": "Nikhat Ali",
-        "start": "9:00 AM",
-        "end": "11:00 AM"
-      },
-      {
-        "course": "ICT 209 / E111",
-        "name": "Object Oriented Programming Using C++",
-        "shortName": "OOPS",
-        "Faculty": "Ms Priyanka Bhutani",
-        "start": "11:00 AM",
-        "end": "12:00 PM"
-      },
-      {
-        "course": "ICT 201 / E111",
-        "name": "Foundations of Computer Science",
-        "shortName": "FCS",
-        "Faculty": "Dr. Rinkaj Goyal",
-        "start": "12:00 PM",
-        "end": "1:00 PM"
-      },
-      {
-        "course": "",
-        "name": "Break",
-        "start": "1:00 PM",
-        "end": "1:30 PM"
-      },
-      {
-        "A": {
-          "course": "ICT 253 / ETL506",
-          "name": "Object Oriented programming using C++ Lab",
-          "shortName": "OOPS LAB",
-          "Faculty": "Ms Priyanka Bhutani",
-          "start": "3:30 PM",
-          "end": "5:30 PM"
-        },
-        "B": {
-          "course": "ICT 257 / ETL312",
-          "name": "Operating Systems Lab ",
-          "shortName": "OS LAB ",
-          "Faculty": "---",
-          "start": "3:30 PM",
-          "end": "5:30 PM"
-        }
-      },
-      {
-        "A": {
-          "course": "ICT 251 / DTL317",
-          "name": "Database Management System Lab",
-          "shortName": "DBMS Lab",
-          "Faculty": "TBA",
-          "start": "3:30 PM",
-          "end": "5:30 PM"
-        },
-        "B": {
-          "course": "ICT 253 / ETL313",
-          "name": "Object Oriented programming using C++ Lab",
-          "shortName": "OOPS LAB",
-          "Faculty": "Ms Priyanka Bhutani",
-          "start": "3:30 PM",
-          "end": "5:30 PM"
-        }
-      }
-    ],
-    "Thursday": [
-      {
-        "course": "BC 121 / E111",
-        "name": "Bridge Course in Mathematics",
-        "shortName": "Maths",
-        "Faculty": "Nikhat Ali",
-        "start": "9:00 AM",
-        "end": "10:00 AM"
-      },
-      {
-        "course": "BC 123 / E111",
-        "name": "Bridge Course in Programming in C",
-        "shortName": "C",
-        "Faculty": "TBA",
-        "start": "10:00 AM",
-        "end": "11:00 AM"
-      },
-      {
-        "course": "ICT 201 / E111",
-        "name": "Foundations of Computer Science",
-        "shortName": "FCS",
-        "Faculty": "Dr. Rinkaj Goyal",
-        "start": "11:00 AM",
-        "end": "1:00 PM"
-      },
-      {
-        "course": "",
-        "name": "Break",
-        "start": "1:00 PM",
-        "end": "1:30 PM"
-      },
-      {
-        "course": "ICT 207 / E111",
-        "name": " Database Management System",
-        "shortName": "DBMS",
-        "Faculty": "TBA",
-        "start": "1:30 PM",
-        "end": "2:30 PM"
-      },
-      {
-        "course": "ICT 203 / E111",
-        "name": "Operating Systems",
-        "shortName": "OS",
-        "Faculty": "---",
-        "start": "2:30 PM",
-        "end": "3:30 PM"
-      },
-      {
-        "course": "ICT 201 / E111",
-        "name": "Foundations of Computer Science",
-        "shortName": "FCS",
-        "Faculty": "Dr. Rinkaj Goyal",
-        "start": "3:30 PM",
-        "end": "4:30 PM"
-      },
-      {
-        "course": "ICT 205 / E111",
-        "name": "Digital Logic and Computer Design",
-        "shortName": "DLCD",
-        "Faculty": "Ms. P. Choudhary",
-        "start": "4:30 PM",
-        "end": "5:30 PM"
-      }
-    ],
-    "Friday": [
-      {
-        "course": "BC 123 / E111",
-        "name": "Bridge Course in Programming in C",
-        "shortName": "C",
-        "Faculty": "---",
-        "start": "9:00 AM",
-        "end": "11:00 AM"
-      },
-      {
-        "course": "ICT 211 / E111",
-        "name": "Data Structures",
-        "shortName": "DS",
-        "Faculty": "Navin Rajpal",
-        "start": "11:00 AM",
-        "end": "1:00 PM"
-      },
-      {
-        "course": "",
-        "name": "Break",
-        "start": "1:00 PM",
-        "end": "1:30 PM"
-      },
-      {
-        "course": "ICT 209 / E111",
-        "name": "Object Oriented Programming Using C++",
-        "shortName": "OOPS",
-        "Faculty": "Ms Priyanka Bhutani",
-        "start": "1:30 PM",
-        "end": "3:30 PM"
-      },
-      {
-        "course": "ICT 207 / E111",
-        "name": "TBA",
-        "shortName": "DBMS",
-        "Faculty": "---",
-        "start": "3:30 PM",
-        "end": "4:30 PM"
-      },
-      {
-        "course": "ICT 205 / E111",
-        "name": "Digital Logic and Computer Design",
-        "shortName": "DLCD",
-        "Faculty": "Ms. P. Choudhary",
-        "start": "4:30 PM",
-        "end": "5:30 PM"
-      }
-    ]
-  };
+    }
+  ],
+  "Thursday": [
+    {
+      "course": "BC 121 / E111",
+      "name": "Bridge Course in Mathematics",
+      "shortName": "Maths",
+      "Faculty": "Nikhat Ali",
+      "start": "9:00 AM",
+      "end": "10:00 AM"
+    },
+    {
+      "course": "BC 123 / E111",
+      "name": "Bridge Course in Programming in C",
+      "shortName": "C",
+      "Faculty": "TBA",
+      "start": "10:00 AM",
+      "end": "11:00 AM"
+    },
+    {
+      "course": "ICT 201 / E111",
+      "name": "Foundations of Computer Science",
+      "shortName": "FCS",
+      "Faculty": "Dr. Rinkaj Goyal",
+      "start": "11:00 AM",
+      "end": "1:00 PM"
+    },
+    {
+      "course": "",
+      "name": "Break",
+      "start": "1:00 PM",
+      "end": "1:30 PM"
+    },
+    {
+      "course": "ICT 207 / E111",
+      "name": " Database Management System",
+      "shortName": "DBMS",
+      "Faculty": "TBA",
+      "start": "1:30 PM",
+      "end": "2:30 PM"
+    },
+    {
+      "course": "ICT 203 / E111",
+      "name": "Operating Systems",
+      "shortName": "OS",
+      "Faculty": "---",
+      "start": "2:30 PM",
+      "end": "3:30 PM"
+    },
+    {
+      "course": "ICT 201 / E111",
+      "name": "Foundations of Computer Science",
+      "shortName": "FCS",
+      "Faculty": "Dr. Rinkaj Goyal",
+      "start": "3:30 PM",
+      "end": "4:30 PM"
+    },
+    {
+      "course": "ICT 205 / E111",
+      "name": "Digital Logic and Computer Design",
+      "shortName": "DLCD",
+      "Faculty": "Ms. P. Choudhary",
+      "start": "4:30 PM",
+      "end": "5:30 PM"
+    }
+  ],
+  "Friday": [
+    
+    {
+      "course": "BC 123 / E111",
+      "name": "Bridge Course in Programming in C",
+      "shortName": "C",
+      "Faculty": "---",
+      "start": "9:00 AM",
+      "end": "11:00 AM"
+    },
+
+    {
+      "course": "ICT 211 / E111",
+      "name": "Data Structures",
+      "shortName": "DS",
+      "Faculty": "Navin Rajpal",
+      "start": "11:00 AM",
+      "end": "1:00 PM"
+    },
+    {
+      "course": "",
+      "name": "Break",
+      "start": "1:00 PM",
+      "end": "1:30 PM"
+    },
+    {
+      "course": "ICT 209 / E111",
+      "name": "Object Oriented Programming Using C++",
+      "shortName": "OOPS",
+      "Faculty": "Ms Priyanka Bhutani",
+      "start": "1:30 PM",
+      "end": "3:30 PM"
+    },
+    {
+      "course": "ICT 207 / E111",
+      "name": "TBA",
+      "shortName": "DBMS",
+      "Faculty": "---",
+      "start": "3:30 PM",
+      "end": "4:30 PM"
+    },
+     {
+      "course": "ICT 205 / E111",
+      "name": "Digital Logic and Computer Design",
+      "shortName": "DLCD",
+      "Faculty": "Ms. P. Choudhary",
+      "start": "4:30 PM",
+      "end": "5:30 PM"
+    }]
+    }
+    
+
 
 const DaysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -332,7 +339,7 @@ const getNextClass = function (Group, current, dayIndex) {
 
 function isCurrentTimeWithinRange(type, periodStart, periodEnd) {
   let [hour, min] = [new Date().getHours(), new Date().getMinutes()];
-  // let [hour, min] = [8,30];
+  // let [hour, min] = [13,40];
   const currentMinutes = hour * 60 + min;
 
   const [Shour, SminPeriod] = periodStart.split(' ');
@@ -349,7 +356,6 @@ function isCurrentTimeWithinRange(type, periodStart, periodEnd) {
 
   const endMinutes = endHour * 60 + endMin - 1;
 
-  
   if(type==='isClassDone') {
     return (currentMinutes <= startMinutes );
   }
